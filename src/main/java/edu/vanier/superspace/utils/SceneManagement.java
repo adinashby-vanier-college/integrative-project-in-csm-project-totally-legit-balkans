f6d6423d16db485de11cc84ab569fbab434d293c
kinqxz <jamesericgamertv@gmail.com>
2025-03-15 00:31:08 -0400

    Added the controller to scene management

--- src/main/java/edu/vanier/superspace/utils/SceneManagement.java
+++ src/main/java/edu/vanier/superspace/utils/SceneManagement.java
@@ -1,15 +1,13 @@
 package edu.vanier.superspace.utils;
 
 import edu.vanier.superspace.Application;
-import edu.vanier.superspace.controllers.AstralCreationFXMLController;
 import edu.vanier.superspace.controllers.MainMenuFXMLController;
-import edu.vanier.superspace.controllers.SettingsFXMLController;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class SceneManagement {
     public static void loadScene(Scenes sceneEnum) {
-        Parent parent = FXML.loadFXML(sceneEnum.getFilepath());
+        Parent parent = FXML.loadFXML(sceneEnum.getFilepath(), new MainMenuFXMLController());
         Scene scene = new Scene(parent, RenderDimensions.getApplicationTargetWidth(), RenderDimensions.getApplicationTargetHeight());
         Application.getPrimaryStage().setScene(scene);
         Application.getPrimaryStage().sizeToScene();
