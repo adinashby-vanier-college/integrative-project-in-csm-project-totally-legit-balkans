3983b7919938690f6299c716aba9e5872329d6cd
c-marza <149026229+c-marza@users.noreply.github.com>
2025-03-17 12:44:42 -0400

    Made the Newtonian gravitational calculation for physics and assigned it to update the bodies while the simulation runs.

--- src/main/java/edu/vanier/superspace/simulation/components/Ridigbody.java
+++ src/main/java/edu/vanier/superspace/simulation/components/Ridigbody.java
@@ -1,4 +1,30 @@
 package edu.vanier.superspace.simulation.components;
 
-public class Ridigbody extends Component  {
+import edu.vanier.superspace.mathematics.Vector2;
+import edu.vanier.superspace.simulation.Tickable;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RidigBody extends Component implements Tickable  {
+    
+    
+    private Vector2 velocity = Vector2.of(0, 0);
+    private double mass = 1.0;
+    private Vector2 acceleration = Vector2.of(0, 0);
+    
+    public void addForce(Vector2 forceVector){
+        
+        acceleration.addAssign(forceVector.divide(mass));
+        
+    }
+
+    @Override
+    public void onUpdate(double deltaTime) {
+        
+        velocity.addAssign(acceleration.divide(deltaTime));
+        acceleration = Vector2.of(0,0);
+        
+    }
+    
 }
