c40e2d648fde268414e90ebe58b9c2cd1a43f044
c-marza <149026229+c-marza@users.noreply.github.com>
2025-05-03 20:35:15 -0400

    Finalized testing and small physics adjustments

--- src/main/java/edu/vanier/superspace/mathematics/Physics.java
+++ src/main/java/edu/vanier/superspace/mathematics/Physics.java
@@ -12,22 +12,6 @@ public class Physics {
      * @param entity1 the first entity
      * @param entity2 the second entity
      */
-    public static void computeAndApplyNewtonGravitation(PhysicsEntity entity1, PhysicsEntity entity2) {
-        
-        Vector2 totalForce1_2 = Vector2.of(0, 0);
-        Vector2 totalForce2_1 = Vector2.of(0, 0);
-        
-        Vector2 distance = entity1.getTransform().getPosition().subtract(entity2.getTransform().getPosition());
-        
-        totalForce1_2 = distance.normalized().multiply((Constants.GRAVITATIONAL_CONSTANT * 
-                entity1.getRigidBody().getMass() *
-                entity2.getRigidBody().getMass() )/ Math.pow(distance.magnitude(), 2));
-        
-        totalForce2_1 = totalForce1_2.negate();
-        
-        entity1.getRigidBody().addForce(totalForce1_2);
-        entity2.getRigidBody().addForce(totalForce2_1);
-    }
 
     /**
      * Application of the law of universal gravitation with the simulation gravitational constant
@@ -41,11 +25,18 @@ public class Physics {
         if (distanceMag < 2500) {
            // distanceMag = Math.max(5, Math.min(distanceMag, 25));
 
-            Vector2 force = distance.normalized().multiply((Constants.SIMULATION_GRAVITATIONAL_CONSTANT *
-                    entity2.getRigidBody().getMass() *
-                    entity1.getRigidBody().getMass()) / Math.pow(distanceMag, 2));
+           
+           
+            Vector2 force = distance.normalized().multiply(computeForce(distanceMag, entity1.getAstralBody().getMass(), entity2.getAstralBody().getMass()));
 
             entity1.getRigidBody().addForce(force);
         }
+    }
+    
+    public static double computeForce(double distanceMag, double firstMass,
+            double secondMass){
+        
+        return (Constants.SIMULATION_GRAVITATIONAL_CONSTANT * firstMass * secondMass) / Math.pow(distanceMag, 2);
+        
     }
 }
